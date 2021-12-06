f465bf7d950be298aea1ab5efafaee14ca4140ae
MagicAtom <1263750383@qq.com>
2021-12-06 11:29:18 +0800

    feat: a delay test tool added in scene.

--- src/scene.cpp
+++ src/scene.cpp
@@ -83,7 +83,7 @@ void Scene::SetObjs()
 {
     // Compile shaders
     shader = new Shader(vs,fs);
-
+    textShader = new Shader("../src/Shaders/text.vs","../src/Shaders/text.fs");
     // Bind buffer
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
@@ -99,6 +99,18 @@ void Scene::SetObjs()
     // normal attribute
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    
+    loadFonts();
+    // This is for the text
+    glGenVertexArrays(1, &tVAO);
+    glGenBuffers(1, &tVBO);
+    glBindVertexArray(tVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, tVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
 }
 
 void Scene::SetCallback()
@@ -146,7 +158,6 @@ void Scene::DrawScene()
 
         // be sure to activate shader when setting uniforms/drawing objects
         shader->use();
-
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera->Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
         glm::mat4 view = camera->GetViewMatrix();
@@ -161,6 +172,16 @@ void Scene::DrawScene()
         // render the cube
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        textShader->use();
+        glm::mat4 tproj = glm::ortho(0.0f, static_cast<GLfloat>(SCR_WIDTH), 0.0f, static_cast<GLfloat>(SCR_HEIGHT));
+        textShader->setMat4("projection",tproj);
+        auto ms = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
+        std::string ms_text = std::to_string(ms.count());
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        renderText(ms_text,25.0f, 25.0f, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
+        glDisable(GL_BLEND);
         GLubyte buf[SCR_WIDTH*SCR_HEIGHT*3] = {0};
         glReadPixels(0,0,SCR_WIDTH,SCR_HEIGHT,GL_RGB,GL_UNSIGNED_BYTE,buf);
         Notify(buf);
@@ -182,6 +203,107 @@ void Scene::Terminate()
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glfwTerminate();
+}
+
+void Scene::loadFonts()
+{
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+
+    FT_Face face;
+    if (FT_New_Face(ft, "../src/fonts/notomono.ttf", 0, &face))
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+    
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    // Disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); 
+    // we only bind numbers
+    for (GLubyte c = 48; c < 57; c++)
+    {
+        // Load character glyph 
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // Generate texture
+        GLuint texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // Set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // Now store character for later use
+        Character character = {
+            texture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            static_cast<GLuint>(face->glyph->advance.x)
+        };
+        Characters.insert(std::pair<GLchar, Character>(c, character));
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // Destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+}
+
+void Scene::renderText(std::string text,GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+    textShader->use();
+    glUniform3f(glGetUniformLocation(textShader->ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(tVAO);
+
+    // 遍历文本中所有的字符
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        GLfloat xpos = x + ch.Bearing.x * scale;
+        GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        GLfloat w = ch.Size.x * scale;
+        GLfloat h = ch.Size.y * scale;
+        // 对每个字符更新VBO
+        GLfloat vertices[6][4] = {
+            { xpos,     ypos + h,   0.0, 0.0 },            
+            { xpos,     ypos,       0.0, 1.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+
+            { xpos,     ypos + h,   0.0, 0.0 },
+            { xpos + w, ypos,       1.0, 1.0 },
+            { xpos + w, ypos + h,   1.0, 0.0 }           
+        };
+        // 在四边形上绘制字形纹理
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // 更新VBO内存的内容
+        glBindBuffer(GL_ARRAY_BUFFER, tVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); 
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // 绘制四边形
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // 更新位置到下一个字形的原点，注意单位是1/64像素
+        x += (ch.Advance >> 6) * scale; // 位偏移6个单位来获取单位为像素的值 (2^6 = 64)
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
--- src/scene.h
+++ src/scene.h
@@ -10,10 +10,17 @@
 #include "shader.h"
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
-
+#include <ft2build.h>
+#include FT_FREETYPE_H
 #include "streamer.h"
-#include <mutex>
-#include <thread>
+#include<chrono>
+
+struct Character {
+    GLuint TextureID;   // ID handle of the glyph texture
+    glm::ivec2 Size;    // Size of glyph
+    glm::ivec2 Bearing;  // Offset from baseline to left/top of glyph
+    GLuint Advance;    // Horizontal offset to advance to next glyph
+};
 
 class Scene {
 public:
@@ -30,14 +37,18 @@ private:
     float prev_time;
     float delta_t;
     void Notify(uint8_t* buf);
+    void loadFonts();
+    void renderText(std::string text,GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color);
     Streamer* streamer_; // This can be changed to a list
 private:
     GLFWwindow* window;
-    unsigned int VBO;
-    unsigned int VAO;
+    unsigned int VBO,tVBO;
+    unsigned int VAO,tVAO;
     Shader* shader;
+    Shader* textShader;
     Camera* camera;
     glm::vec3 lightPos;
+    std::map<GLchar, Character> Characters;
 };
 
 
